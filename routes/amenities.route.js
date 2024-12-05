0c66f06c0ba4061eada9e257c45859fb9611faa8
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-12-05 20:55:53 +0700

    modify endpoint in amenities.route.js

--- routes/amenities.route.js
+++ routes/amenities.route.js
@@ -3,7 +3,7 @@ const { database } = require("../database");
 const { generateUUIDV4 } = require("../utils/idManager");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/rooms", async (req, res) => {
     let {ten, moTa} = req.body
     try {
         // CALL (IN ID VARCHAR(50), IN Ten VARCHAR(255), IN MoTa TEXT)
@@ -17,6 +17,20 @@ router.post("/", async (req, res) => {
     }
 });
 
+
+router.post("/branch", async (req, res) => {
+    let {ten, moTa} = req.body
+    try {
+        // CALL (IN ID VARCHAR(50), IN Ten VARCHAR(255), IN MoTa TEXT)
+        const QUERY = `CALL ThemTienNghiPhong ('${generateUUIDV4()}', '${ten}', '${moTa}')`
+        
+        const [result] = await database.query(QUERY)
+        res.status(201).send({status: "success", message: "Thêm tiện nghi thành công"})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({status: "failed", error: error.message})
+    }
+});
 
 
 module.exports = router;
