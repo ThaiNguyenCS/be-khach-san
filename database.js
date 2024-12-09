03771c772fa7d1f4d811ba2b1b2ef545ae444b6b
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-12-09 21:56:55 +0700

    deploy

--- database.js
+++ database.js
@@ -5,6 +5,7 @@ const database = sql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT
 });
 
 module.exports = { database };
--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const { differenceInBusinessDays } = require("date-fns");
-
-console.log(differenceInBusinessDays(new Date("2024-12-10 22:00:00"), new Date("2024-12-09")));
+function generateNumbers() {
+    for (let i = 1; i <= 3; i++) { // 3 sets (101-120, 201-220, 301-320)
+        const start = i * 100 + 1;
+        const end = start + 20;
+        for (let number = start; number < end; number++) {
+            console.log(number)
+        }
+    }
+}
+generateNumbers()
