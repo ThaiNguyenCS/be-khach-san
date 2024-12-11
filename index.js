522f8503055568dd0fcea9ae77e19d7b1a7dee20
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-12-11 18:08:15 +0700

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const discountRouter = require("./routes/discount.route");
 var cors = require("cors");
 const morgan = require("morgan");
 const CORS_OPTION = {
-    origin: ["http://localhost:3000, https://dbd1-115-79-30-18.ngrok-free.app"],
+    origin: ["http://localhost:3000"],
     credentials: true,
 };
 app.use(cors(CORS_OPTION));
